1a4e5a8b62f2386bb2cc4bdf5449d6a1ba2bc0ab
alamin-karno <amin15-1951@diu.edu.bd>
2024-01-17 21:17:30 +0600

    Bounce around

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:brick_breaker/src/brick_breaker.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  final game = FlameGame();
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+
+  final game = BrickBreaker();
 
   runApp(GameWidget(game: game));
 }
---
+++ lib/src/brick_breaker.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+import 'dart:math' as math;
+
+import 'package:brick_breaker/src/components/components.dart';
+import 'package:brick_breaker/src/config.dart';
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+
+class BrickBreaker extends FlameGame with HasCollisionDetection {
+  BrickBreaker()
+      : super(
+          camera: CameraComponent.withFixedResolution(
+            width: gameWidth,
+            height: gameHeight,
+          ),
+        );
+
+  final rand = math.Random();
+  double get width => size.x;
+  double get height => size.y;
+
+  @override
+  FutureOr<void> onLoad() {
+    camera.viewfinder.anchor = Anchor.topLeft;
+
+    world.add(PlayArea());
+
+    world.add(Ball(
+      position: size / 2,
+      radius: ballRadius,
+      velocity:
+          Vector2((rand.nextDouble() - 0.5) * width, height * 0.2).normalized()
+            ..scale(height / 4),
+    ));
+    return super.onLoad();
+  }
+
+  @override
+  bool get debugMode => true;
+}
